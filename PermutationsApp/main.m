591edb74df94a89c9071630f61200c90f2422d80
Marijn Schilling <marijnschilling@gmail.com>
2014-06-10 20:42:41 +0200

    Empty Xcode file

---
+++ PermutationsApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PermutationsApp
+//
+//  Created by Marijn Schilling on 10/06/14.
+//  Copyright (c) 2014 ACDME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRMAppDelegate class]));
+    }
+}
