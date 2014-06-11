547e7c9ba6f72b885969b75a551934418576885f
Marijn Schilling <marijnschilling@gmail.com>
2014-06-11 20:33:27 +0200

    Create operation permutations

--- PermutationsApp/Source/Application start-up/PRMCreatePermutationLists.m
+++ PermutationsApp/Source/Application start-up/PRMCreatePermutationLists.m
@@ -4,6 +4,7 @@
 @property(nonatomic, strong) NSArray *numbers;
 @property(nonatomic, strong) NSArray *operators;
 @property(nonatomic, strong) NSMutableArray *numberPermutations;
+@property(nonatomic, strong) NSMutableArray *operationPermutations;
 @end
 
 @implementation PRMCreatePermutationLists
@@ -14,9 +15,10 @@
 
     if (self)
     {
-        self.numbers = @[@2, @4,@8, @16, @32];
+        self.numbers = @[@2, @4, @8, @16, @32];
         self.operators = @[@"+", @"-", @"*", @"/"];
         self.numberPermutations = [NSMutableArray array];
+        self.operationPermutations = [NSMutableArray array];
     }
 
     return self;
@@ -27,31 +29,49 @@
     NSMutableArray *result = [NSMutableArray array];
     NSMutableArray *leftover = [self.numbers mutableCopy];
     [self findNumberPermutationsRecursivelyfromLeftover:leftover andResult:result];
-    NSLog(@"Found %@", self.numberPermutations);
-//    [self findOperatorPermutationsRecursively]
+
+    [self findOperatorPermutations];
+    NSLog(@"number of operation permutations %u", [self.operationPermutations count]);
 
     [self.delegate foundAllPermutations];
 }
 
-
-
 - (void)findNumberPermutationsRecursivelyfromLeftover:(NSMutableArray *)leftover andResult:(NSMutableArray *)result
 {
-    if([leftover count] == 0)
+    if ([leftover count] == 0)
     {
         [self.numberPermutations addObject:result];
     }
     else
     {
-        for(NSUInteger i = 0; i < [leftover count]; i++)
+        for (NSUInteger i = 0; i < [leftover count]; i++)
         {
-            NSMutableArray * leftoverCopy = [leftover mutableCopy];
-            NSMutableArray * resultCopy = [result mutableCopy];
+            NSMutableArray *leftoverCopy = [leftover mutableCopy];
+            NSMutableArray *resultCopy = [result mutableCopy];
 
             [resultCopy addObject:leftoverCopy[i]];
             [leftoverCopy removeObjectAtIndex:i];
 
             [self findNumberPermutationsRecursivelyfromLeftover:leftoverCopy andResult:resultCopy];
+        }
+    }
+}
+
+- (void)findOperatorPermutations
+{
+    for (NSUInteger i = 0; i < [self.operators count]; i++)
+    {
+        for (NSUInteger j = 0; j < [self.operators count]; j++)
+        {
+            for (NSUInteger k = 0; k < [self.operators count]; k++)
+            {
+                for (NSUInteger l = 0; l < [self.operators count]; l++)
+                {
+                    NSArray *operationsPermutation;
+                    operationsPermutation = @[@[self.operators[i]], @[self.operators[j]], @[self.operators[k]], @[self.operators[l]]];
+                    [self.operationPermutations addObject:operationsPermutation];
+                }
+            }
         }
     }
 }
